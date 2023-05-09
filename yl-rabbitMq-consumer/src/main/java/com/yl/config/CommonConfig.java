3c792e36fe51fb6ee076b56c5ba4087798980f11
杨磊 <lei.yang@xbongbong.com>
2023-05-09 09:58:32 +0800

    feat:依赖整理

---
+++ yl-rabbitMq-consumer/src/main/java/com/yl/config/CommonConfig.java
@@ -0,0 +1,20 @@
+package com.yl.config;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.context.annotation.Bean;
+
+//@Configuration
+public class CommonConfig {
+    @Bean
+    public DirectExchange simpleDirect(){
+        return new DirectExchange("simple.direct",true,false);
+    }
+
+    @Bean
+    public Queue simpleQueue(){
+        return QueueBuilder.durable("simple.queue").build();
+    }
+}
+
