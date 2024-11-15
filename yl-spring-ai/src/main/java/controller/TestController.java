fbf41c5d0b9b2be7e8738b588db3b7a3cfd0569e
杨磊 <lei.yang@xbongbong.com>
2024-11-15 14:42:52 +0800

    feat:测试springAi

---
+++ yl-spring-ai/src/main/java/controller/TestController.java
@@ -0,0 +1,27 @@
+package controller;
+
+
+import org.springframework.ai.client.AiClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/springAi")
+public class TestController {
+
+    private final AiClient aiClient;
+
+    public TestController(AiClient aiClient) {
+        this.aiClient = aiClient;
+    }
+
+
+    @GetMapping("/ai/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return aiClient.generate(message);
+    }
+
+
+}
