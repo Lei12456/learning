48abd4f4ad2d6d657ad1f3d6e56b8e08c1e8a1aa
纳尔 <17637209763@163.com>
2026-02-26 16:19:21 +0800

    pom整理

---
+++ yl-ai-code/src/main/java/com/yl/ynaicode/YLAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.yl.ynaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YLAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YLAiCodeApplication.class, args);
+    }
+
+}
---
+++ yl-ai-code/src/test/java/com/yl/ynaicode/YnAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yl.ynaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
